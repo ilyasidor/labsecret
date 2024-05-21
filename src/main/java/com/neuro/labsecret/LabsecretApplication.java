93ee6c43e98540e3bb88aa1cf72fab4fba1f5cc4
ilyasidor <deadboost.666@gmail.com>
2024-05-21 13:05:36 +0900

    raw
    asf

--- src/main/java/com/neuro/labsecret/LabsecretApplication.java
+++ src/main/java/com/neuro/labsecret/LabsecretApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabsecretApplication {
 
-	//asdfasf
 	public static void main(String[] args) {
 		SpringApplication.run(LabsecretApplication.class, args);
 	}
