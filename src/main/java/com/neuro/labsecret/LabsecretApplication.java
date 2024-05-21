dfa5914907f9c732fdb2e355fccead6838ede0d9
ilyasidor <deadboost.666@gmail.com>
2024-05-21 13:02:55 +0900

    56u
    rytu

--- src/main/java/com/neuro/labsecret/LabsecretApplication.java
+++ src/main/java/com/neuro/labsecret/LabsecretApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LabsecretApplication {
-	
+
+	//asdfasf
 	public static void main(String[] args) {
 		SpringApplication.run(LabsecretApplication.class, args);
 	}
