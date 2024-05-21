1185ca5cb81c9c09e77773e606d67288e38debf1
ilyasidor <deadboost.666@gmail.com>
2024-05-21 12:52:54 +0900

    Initial commit

---
+++ src/main/java/com/neuro/labsecret/LabsecretApplication.java
@@ -0,0 +1,11 @@
+package com.neuro.labsecret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsecretApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LabsecretApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neuro/labsecret/LabsecretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuro.labsecret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsecretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
